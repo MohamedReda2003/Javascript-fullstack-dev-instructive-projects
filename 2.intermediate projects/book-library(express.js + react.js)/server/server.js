5525e508a8c7033a02824e51a3391c99ac366466
Mohamed Reda <mohamedredazhar2003@gmail.com>
2024-08-08 00:52:24 +0100

    added via upload

---
+++ 2.intermediate projects/book-library(express.js + react.js)/server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+let books = [
+    { id: 1, title: "1984", author: "George Orwell" },
+    { id: 2, title: "To Kill a Mockingbird", author: "Harper Lee" }
+];
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/api/books', (req, res) => {
+    res.json(books);
+});
+
+app.post('/api/books', (req, res) => {
+    const newBook = { id: books.length + 1, title: req.body.title, author: req.body.author };
+    books.push(newBook);
+    res.json(newBook);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
