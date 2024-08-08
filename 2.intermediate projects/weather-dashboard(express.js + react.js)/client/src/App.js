10ccbc78f867445f4c21b141c94b9349bf992a17
Mohamed Reda <mohamedredazhar2003@gmail.com>
2024-08-08 11:36:33 +0100

    added via upload

--- 2.intermediate projects/weather-dashboard(express.js + react.js)/client/src/App.js
+++ 2.intermediate projects/weather-dashboard(express.js + react.js)/client/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import WeatherCard from './WeatherCard.js';
 import $ from 'jquery';
-
+API_KEY = "YOUR_API_KEY";
+//You can get your APi key from the website: https://home.openweathermap.org/users/sign_up
 const App = () => {
     const [weatherData, setWeatherData] = useState([]);
 
     const fetchWeather = () => {
-        $.get('https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY', function(data) {
+        $.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid={API_KEY}`, function(data) {
             setWeatherData([data]);
         });
     };
