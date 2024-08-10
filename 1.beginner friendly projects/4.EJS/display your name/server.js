e3ec139eca50460022bf728871e000931420840a
Mohamed Reda <mohamedredazhar2003@gmail.com>
2024-08-10 12:20:35 +0100

    added via upload

---
+++ 1.beginner friendly projects/4.EJS/display your name/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+//import { name } from 'ejs';
+
+
+
+const app = express();
+const port = 5000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    let chosen_name = "Richard";
+    const inputString = req.body.inputString;
+    console.log(`Input is ${inputString}`);
+    res.render("index.ejs",{name1:chosen_name,name2:inputString});
+});
+app.post("/", (req, res) => {
+    let chosen_name = "Richard";
+    const inputString = req.body.inputString;
+    console.log(`Input is ${inputString}`);
+    res.render("index.ejs",{name1:chosen_name,name2:inputString});
+});
+
+app.listen(port, () => {
+    console.log(`The server is running on http://localhost:${port}`);
+});
